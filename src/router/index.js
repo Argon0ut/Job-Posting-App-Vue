cd8ca05f1c6ae5222207f882fbe2006b20baf9a9
Argon0ut <nusupoversultan228@gmail.com>
2025-07-09 23:40:33 +0500

    repository setup

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import JobListings from '@/components/JobListings.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import JobView from '@/views/JobView.vue';
+import AddJobView from '@/views/AddJobView.vue';
+import EditJobView from '@/views/EditJobView.vue';
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: JobListings,
+        },
+        {
+            path: '/jobs/:id',
+            name: 'job',
+            component: JobView,
+        },
+        {
+            path: '/jobs/edit/:id',
+            name: 'edit-job',
+            component: EditJobView,
+        },
+        {
+            path: '/jobs/add',
+            name: 'add-job',
+            component: AddJobView,
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component: NotFoundView,
+        }
+        
+
+    ]
+
+})
+
+export default router;
